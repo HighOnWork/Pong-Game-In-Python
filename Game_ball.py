aa0483879cb0ad763f826b8e00ee69b01cf13236
Muhammad Ahmad <highonworking@gmail.com>
2025-10-19 18:56:28 +0500

    IT WORKS

--- Game_ball.py
+++ Game_ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 ROTATION_VALUE = 40
 
@@ -10,26 +11,25 @@ class BallForGame(Turtle):
         self.color("white")
         self.shape("circle")
         self.x_move = 10
-        self.right(ROTATION_VALUE)
+        self.flag = False
 
     def ball_move(self):
-        if self.x_move == 10:
-            self.forward(self.x_move)
-        else:
-            self.backward(self.x_move)
+        self.forward(self.x_move)
     
-    def ball_physics(self, side):
-        new_angle = 180 - ROTATION_VALUE
-        if new_angle == 0:
-            new_angle = 90
-        if side == 'right':
-            if new_angle <= 90:
-                self.left(new_angle)
-            else:
-                self.right(new_angle)
-        else:
-            if new_angle <= 90:
-                self.right(new_angle)
-            else:
-                self.left(new_angle)
-        self.x_move *= -1
+    def ball_physics(self):
+        new_heading = 180 - self.heading()
+        new_heading += random.randint(-10, 10)
+        self.setheading(new_heading % 360)
+
+    def if_on_top_or_bottom(self):
+        if self.ycor() <= -290:
+            self.flag = True
+            new_heading = 150
+            new_heading += random.randint(-10, 10)
+        elif self.ycor() >= 290:
+            self.flag = True
+            new_heading = 200
+            new_heading += random.randint(-10, 10)
+        if self.flag:
+            self.setheading(new_heading)
+            self.flag = False
--- main.py
+++ main.py
@@ -13,12 +13,13 @@ ping_pong_ball = BallForGame()
 score_on_both_sides = ScoreboardGame()
 
 def game_loop():
+    ping_pong_ball.if_on_top_or_bottom()
     Racket_left.coordinates('left')
     Racket_right.coordinates('right')
     if Racket_left.rectangle_top_y >= ping_pong_ball.ycor() >= Racket_left.rectangle_bottom_y and Racket_left.rectangle_other_x >= ping_pong_ball.xcor() >= Racket_left.rectangle_x:
-        ping_pong_ball.ball_physics('left')
+        ping_pong_ball.ball_physics()
     if Racket_right.rectangle_top_y >= ping_pong_ball.ycor() >= Racket_right.rectangle_bottom_y and Racket_right.rectangle_other_x >= ping_pong_ball.xcor() >= Racket_right.rectangle_x:
-        ping_pong_ball.ball_physics('right')
+        ping_pong_ball.ball_physics()
     new_screen.screen.onkeypress(Racket_left.move_up, "w")
     new_screen.screen.onkeypress(Racket_left.move_down, "s")
     new_screen.screen.onkeypress(Racket_right.move_up, "Up")
